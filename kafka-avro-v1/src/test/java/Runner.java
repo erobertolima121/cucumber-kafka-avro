efe032c40af267d7f0b75be5c4e2cc05dd8cd435
erobertolima121 <e.robertolima121@gmail.com>
2020-08-18 20:07:15 -0300

    limpeza de estrutura

--- kafka-avro-v1/src/test/java/Runner.java
+++ kafka-avro-v1/src/test/java/Runner.java
@@ -3,7 +3,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\QA\\Itau\\kafka-automation\\project-avro\\cucumber-kafka-avro\\kafka-avro-v1\\src\\main\\resources\\features", tags = "@exemplo",
+@CucumberOptions(features = "src\\main\\resources\\features", tags = "@exemplo",
 glue = "Steps", monochrome = true, dryRun = false)
 public class Runner {
 }
